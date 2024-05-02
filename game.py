e1ea6d9915c0d70af0d40873c4cd07c7194f8a3f
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-05-02 14:14:08 +0300

    camera and background clouds

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ import pygame
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -27,22 +28,30 @@ class Game:
 
         # --- ENTITIES ---
 
-        # Player entity
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))      # The third parameter is the starting position
-        self.movement = [False, False]
-
         # Loading images for entities
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
         }
+
+        # Player entity
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))      # The third parameter is the starting position
+        self.movement = [False, False]
 
         # --- GAME MAP ---
 
+        self.clouds = Clouds(self.assets['clouds'], count=16)
+
         self.tilemap = Tilemap(self, tile_size=16)
+
+        # --- CAMERA ---
+
+        self.scroll = [0, 0]
 
     # --- GAMELOOP ---
 
@@ -53,13 +62,22 @@ class Game:
 
             # --- RENDERING ---
 
-            # Fills the whole screen with this color at the start of every frame to "clean", otherwise all moved sprites would leave traces 
-            self.display.fill((14, 219, 248))
+            # Fills the whole screen with background image at the start of every frame to "clean", otherwise all moved sprites would leave traces 
+            self.display.blit(self.assets['background'], (0, 0))
 
-            self.tilemap.render(self.display)
+            # Moves the camera centering on the player with smoothing
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            # Removes the player character jitter releated to camera movement by removing decimal handling with casting to integer, camera "choppines" remains
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
 
             # --- INPUT READING ---
 
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -37,13 +37,13 @@ class Tilemap:
         return rects
 
     # Off grid tiles rendered before on grid ones
-    def render(self, surf):
+    def render(self, surf, offset=(0, 0)):
         for tile in self.offgrid_tiles:
             # No gridding for offgrid tiles
-            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
 
         for loc in self.tilemap:
             tile = self.tilemap[loc]
             # Renders the tile in specific location using it's type, variant and position. The position is multiplied with the tile size ie. tile grid size.
             # Asset loading is done in game.py
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
